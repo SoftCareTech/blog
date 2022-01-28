7191b7104797cf1568ad0430419f241000e3a080
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-28 19:50:00 +0100

    add pst work 1

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({ children }) => {
+    const blogPost = [
+        { title: 'Blog 1' },
+        { title: 'Blog 2' },
+        { title: 'Blog 3' }
+    ]
 
-    return <BlogContext.Provider value={5} >
+    const [blogPosts, setBlogPosts] = useState(blogPost)
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+    }
+
+    return <BlogContext.Provider value={
+        {
+            data: blogPosts,
+            addBlogPost
+        }
+    } >
         {children}
     </BlogContext.Provider>
 }
--- src/screen/IndexScreen.js
+++ src/screen/IndexScreen.js
@@ -1,13 +1,24 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext)
+    const { data, addBlogPost } = useContext(BlogContext)
+    console.log('data')
+    console.log(data)
 
-    console.log(value)
     return <>
-        <Text> hello Index Screen</Text>
+        <Text>Index Screen</Text>
+        <Button title='Add Post' onPress={addBlogPost} />
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.title}
+            renderItem={({ item }) => {
+                return <Text>{item.title} </Text>
+            }}
+        />
+
     </>
 }
 
