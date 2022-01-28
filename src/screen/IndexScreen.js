eec13ac933018666d9d86f1b3c5ccdc8b77abce1
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-28 21:38:49 +0100

    pstyles on blog index

--- src/screen/IndexScreen.js
+++ src/screen/IndexScreen.js
@@ -1,20 +1,24 @@
 import React, { useContext } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
-import {Context as BlogContext} from '../context/BlogContext'
-
+import { Context as BlogContext } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 const IndexScreen = () => {
     const { state, addBlogPost } = useContext(BlogContext)
-    
+
 
     return <>
-        <Text>Index Screen</Text>
         <Button title='Add Post' onPress={addBlogPost} />
         <FlatList
             data={state}
             keyExtractor={(item) => item.title}
             renderItem={({ item }) => {
-                return <Text>{item.title} </Text>
+                return <View style={styles.row}>
+                    <Text style={styles.title}>{item.title}
+                    </Text>
+                    <Feather name='trash' style={styles.icon} />
+                </View>
+
             }}
         />
 
@@ -23,6 +27,20 @@ const IndexScreen = () => {
 
 
 const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        paddingHorizontal: 10,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
 
 })
 
