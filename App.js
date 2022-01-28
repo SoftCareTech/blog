2e2e4705abc7788ce4dbd38c48b50c6acae2bd77
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-01-28 14:17:54 +0100

    index and context init

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import IndexScreen from './src/screen/IndexScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react'
+const navigator = createStackNavigator({
+    Index: IndexScreen
+}, {
+    initialRouterName: 'Index'
+    , defaultNavigationOptions: {
+        title: "Blog"
+    }
 }
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const App = createAppContainer(navigator)
+export default () => {
+    return <App />
+}
---
+++ src/screen/IndexScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+const IndexScreen = () => {
+
+    return <>
+        <Text> hello Index Screen</Text>
+    </>
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default IndexScreen
+
+
+
+
+
+
